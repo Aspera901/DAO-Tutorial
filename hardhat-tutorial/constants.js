3fe5dc4d04143307a4d0184517e1a92b87f2ef3a
Aspera901 <vodico1@gmail.com>
2022-02-04 12:33:38 -0500

    making my-app a directory

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xB90728bCcc67703a550113a49F1613847F0c51cD";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
